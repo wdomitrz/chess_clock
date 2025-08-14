07e1d4939cbea7fdd3713b976e1d62a0dc9b6fb1
Witalis Domitrz <witekdomitrz@gmail.com>
2025-08-14 22:26:54 +0100

    icon, manifest, sw

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "chess_clock-cache";
+const FILES = [
+  "./",
+  "./app.js",
+  "./icon.svg",
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
